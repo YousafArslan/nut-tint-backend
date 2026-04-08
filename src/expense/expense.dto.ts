bf8c7a09b781a5394d5252fb15abb029a58a8d98
Azwar <tanbits@Tanbitss-MacBook-Pro-3.local>
2026-04-08 11:42:35 +0500

    added date in expense

--- src/expense/expense.dto.ts
+++ src/expense/expense.dto.ts
@@ -2,38 +2,47 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsOptional, IsDateString } from 'class-validator';
 
 export class CreateExpenseDto {
-    @ApiProperty({
-      example: 'Office Supplies',
-    })
-    @IsString()
-    description: string;
-  
-    @ApiProperty({
-      example: 150.75,
-    })
-    @IsNumber()
-    amount: number;
-  }
+  @ApiProperty({
+    description: 'Date for the filter',
+    type: String,
+    required: false,
+    example: '2025-01-01T00:00:00Z',
+  })
+  @IsOptional()
+  @IsDateString()
+  createdAt?: string;
 
+  @ApiProperty({
+    example: 'Office Supplies',
+  })
+  @IsString()
+  description: string;
 
-  export class GetExpensesDto {
-    @ApiProperty({
-      description: 'Start date for the filter',
-      type: String,
-      required: false,
-      example: '2025-02-12T00:00:00Z',
-    })
-    @IsOptional()
-    @IsDateString()
-    startDate?: string;
-  
-    @ApiProperty({
-      description: 'End date for the filter',
-      type: String,
-      required: false,
-      example: '2025-02-12T23:59:59Z',
-    })
-    @IsOptional()
-    @IsDateString()
-    endDate?: string;
-  }
+  @ApiProperty({
+    example: 150.75,
+  })
+  @IsNumber()
+  amount: number;
+}
+
+export class GetExpensesDto {
+  @ApiProperty({
+    description: 'Start date for the filter',
+    type: String,
+    required: false,
+    example: '2025-02-12T00:00:00Z',
+  })
+  @IsOptional()
+  @IsDateString()
+  startDate?: string;
+
+  @ApiProperty({
+    description: 'End date for the filter',
+    type: String,
+    required: false,
+    example: '2025-02-12T23:59:59Z',
+  })
+  @IsOptional()
+  @IsDateString()
+  endDate?: string;
+}
